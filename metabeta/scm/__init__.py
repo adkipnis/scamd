6e51aa62cced29b77a6b5635041ce7dbd98828a3
adkipnis <adkipnis@mailbox.org>
2026-01-03 13:15:07 +0100

    update init

--- metabeta/scm/__init__.py
+++ metabeta/scm/__init__.py
@@ -1,5 +1,6 @@
 from .activations import getActivations
+from .posthoc import getPosthocLayers
 from .causes import CauseSampler
 from .scm import SCM
 
-__all__ = ['getActivations', 'CauseSampler', 'SCM']
+__all__ = ['getActivations', 'getPosthocLayers', 'CauseSampler', 'SCM']

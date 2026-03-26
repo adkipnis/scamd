0e31026aaf88ada50ef97df8de42ed03653134a1
adkipnis <adkipnis@mailbox.org>
2026-03-27 00:35:39 +0100

    init

---
+++ src/api.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+import numpy as np
+
+from .scm import Posthoc, SCM
+
+
+def generate_dataset(**config: Any) -> np.ndarray:
+    """Generate one standardized X-only synthetic dataset."""
+    scm = SCM(**config)
+    x = scm.sample()
+    posthoc = Posthoc(**config)
+    return posthoc(x)

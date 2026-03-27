0add15f71e074a281806ee7ad8634dead5608cf7
adkipnis <adkipnis@mailbox.org>
2026-03-27 11:20:42 +0100

    import

--- scamd/__init__.py
+++ scamd/__init__.py
@@ -1,6 +1,7 @@
 """Convenience exports for dataset generation and SCM models."""
 
 from .api import generate_dataset
+from .plotting import plot_dataset
 from .scm import SCM, Posthoc
 
-__all__ = ['SCM', 'Posthoc', 'generate_dataset']
+__all__ = ['SCM', 'Posthoc', 'generate_dataset', 'plot_dataset']

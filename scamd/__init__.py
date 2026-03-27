b42180269476b5f115b204e7af744da6907a0659
adkipnis <adkipnis@mailbox.org>
2026-03-27 12:10:25 +0100

    update init

--- scamd/__init__.py
+++ scamd/__init__.py
@@ -2,6 +2,23 @@
 
 from .api import generate_dataset
 from .plotting import plot_dataset
+from .presets import (
+    DATASET_PRESETS,
+    POOL_PRESETS,
+    PRESET_LABELS,
+    get_dataset_preset,
+    get_pool_preset,
+)
 from .scm import SCM, Posthoc
 
-__all__ = ['SCM', 'Posthoc', 'generate_dataset', 'plot_dataset']
+__all__ = [
+    'SCM',
+    'Posthoc',
+    'generate_dataset',
+    'plot_dataset',
+    'POOL_PRESETS',
+    'DATASET_PRESETS',
+    'PRESET_LABELS',
+    'get_pool_preset',
+    'get_dataset_preset',
+]
